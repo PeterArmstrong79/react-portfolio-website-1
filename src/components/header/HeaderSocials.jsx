d54b195eb7cea228db3e07591dcd295765b18025
Peter Armstrong <peterarmstrong999@gmail.com>
2022-05-22 12:57:26 +0100

    header social icons in position

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {FaDribbble} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="blank"><FaGithub/></a>
-        <a href="https://dribbble.com" target="blank"><FaDribbble/></a>
+        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com" target="_blank"><FaGithub/></a>
+        <a href="https://dribbble.com" target="_blank"><FaDribbble/></a>
     </div>
   )
 }
